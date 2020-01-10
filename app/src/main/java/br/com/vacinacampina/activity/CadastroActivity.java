c2c610f3d46eb6fe3998462c9f2536a6d1253708
Emerson Sousa <emersonuepb@gmail.com>
2020-01-10 01:40:40 -0300

    finalização login, criação da tela principal

--- app/src/main/java/br/com/vacinacampina/activity/CadastroActivity.java
+++ app/src/main/java/br/com/vacinacampina/activity/CadastroActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -17,6 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import br.com.vacinacampina.R;
 import br.com.vacinacampina.config.FirebaseConfig;
@@ -27,6 +28,10 @@ public class CadastroActivity extends AppCompatActivity {
     public static final String O_CAMPO_NOME_NÃO_PODE_SER_VAZIO = "O campo nome não pode ser vazio.";
     public static final String O_CAMPO_EMAIL_NÃO_PODE_SER_VAZIO = "O campo email não pode ser vazio.";
     public static final String O_CAMPO_SENHA_NÃO_PODE_SER_VAZIO = "O campo senha não pode ser vazio.";
+    public static final String UTILIZE_UMA_SENHA_MAIS_FORTE = "Utilize uma senha mais forte!";
+    public static final String POR_FAVOR_DIGITE_UM_EMAIL_VALIDO = "Por favor digite um email valido!";
+    public static final String JÁ_EXISTE_UM_USUÁRIO_COM_ESETE_EMAIL_CADASTRADO = "Já existe um usuário com esete email cadastrado!";
+    public static final String ERRO_AO_CADASTRAR_VERIFIQUE_OS_DADOS_E_TENTE_NOVAMENTE = "Erro ao cadastrar, verifique os dados e tente novamente.";
     private EditText campoNome, campoEmail, campoSenha;
     private Button btnCadastrar;
     private ProgressBar progressBar;
@@ -38,8 +43,8 @@ public class CadastroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro);
 
         campoNome = findViewById(R.id.editText_nome);
-        campoEmail = findViewById(R.id.editText_email);
-        campoSenha = findViewById(R.id.editText_senha);
+        campoEmail = findViewById(R.id.editText_email_login);
+        campoSenha = findViewById(R.id.editText_senha_login);
         btnCadastrar = findViewById(R.id.button_cadastrar);
         progressBar = findViewById(R.id.progressBar_cadastro);
 
@@ -71,8 +76,22 @@ public class CadastroActivity extends AppCompatActivity {
                             setContentView(R.layout.activity_sucesso_cadastro);
                         } else {
                             progressBar.setVisibility(View.GONE);
-                            Toast.makeText(CadastroActivity.this
-                                    , task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            try {
+                                throw task.getException();
+                            }catch (FirebaseAuthWeakPasswordException authWeakPasswordException){
+                                Toast.makeText(CadastroActivity.this
+                                        , UTILIZE_UMA_SENHA_MAIS_FORTE, Toast.LENGTH_LONG).show();
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                Toast.makeText(CadastroActivity.this
+                                        , POR_FAVOR_DIGITE_UM_EMAIL_VALIDO, Toast.LENGTH_LONG).show();
+                            } catch (FirebaseAuthUserCollisionException userCollisionException){
+                                Toast.makeText(CadastroActivity.this
+                                        , JÁ_EXISTE_UM_USUÁRIO_COM_ESETE_EMAIL_CADASTRADO, Toast.LENGTH_LONG).show();
+                            } catch (Exception e) {
+                                Toast.makeText(CadastroActivity.this
+                                        , ERRO_AO_CADASTRAR_VERIFIQUE_OS_DADOS_E_TENTE_NOVAMENTE, Toast.LENGTH_LONG).show();
+                            }
+
                         }
                     }
                 });
--- app/src/main/java/br/com/vacinacampina/activity/LoginActivity.java
+++ app/src/main/java/br/com/vacinacampina/activity/LoginActivity.java
@@ -1,21 +1,105 @@
 package br.com.vacinacampina.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 import br.com.vacinacampina.R;
+import br.com.vacinacampina.config.FirebaseConfig;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String O_CAMPO_NOME_NÃO_PODE_SER_VAZIO = "O campo nome não pode ser vazio.";
+    public static final String O_CAMPO_EMAIL_NÃO_PODE_SER_VAZIO = "O campo email não pode ser vazio.";
+    public static final String O_CAMPO_SENHA_NÃO_PODE_SER_VAZIO = "O campo senha não pode ser vazio.";
+    public static final String EMAIL_NÃO_CADASTRADO = "Email não cadastrado!";
+    public static final String SENHA_INCORRETA = "Senha Incorreta!";
+    public static final String ERRO_AO_REALIZAR_LOGIN_VERIFIQUE_OS_DADOS_E_TENTE_NOVAMENTE = "Erro ao realizar login, verifique os dados e tente novamente!";
+    private EditText campoEmail,campoSenha;
+    private Button buttonEntrar;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editText_email_login);
+        campoSenha = findViewById(R.id.editText_senha_login);
+        buttonEntrar = findViewById(R.id.button_entrar);
+        progressBar = findViewById(R.id.progressBar_login);
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validaCampos(campoEmail.getText().toString(), campoSenha.getText().toString())) {
+                    logar();
+                }
+            }
+        });
     }
+
+    /**
+     * Realiza o login na aplicação
+     */
+    private void logar() {
+        progressBar.setVisibility(View.VISIBLE);
+        FirebaseConfig.getAuth()
+                .signInWithEmailAndPassword(campoEmail.getText().toString(), campoSenha.getText().toString())
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this, TelaPrincipalActivity.class));
+                        }else {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            try {
+                                throw task.getException();
+
+                            }catch (FirebaseAuthInvalidUserException authInvalidUserException){
+                                Toast.makeText(LoginActivity.this,
+                                        EMAIL_NÃO_CADASTRADO, Toast.LENGTH_LONG).show();
+                            }catch (FirebaseAuthInvalidCredentialsException authInvalidCredentialsException){
+                                Toast.makeText(LoginActivity.this,
+                                        SENHA_INCORRETA, Toast.LENGTH_LONG).show();
+                            } catch (Exception e) {
+                                Toast.makeText(LoginActivity.this,
+                                        ERRO_AO_REALIZAR_LOGIN_VERIFIQUE_OS_DADOS_E_TENTE_NOVAMENTE,
+                                        Toast.LENGTH_LONG).show();
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+    }
+
     public void btnCadastrar(View view){
         startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+    private Boolean validaCampos(String campoEmail, String campoSenha) {
+
+        Boolean valido = true;
+         if (campoEmail.isEmpty()){
+            Toast.makeText(LoginActivity.this
+                    , O_CAMPO_EMAIL_NÃO_PODE_SER_VAZIO, Toast.LENGTH_LONG).show();
+            valido = false;
+        } else if (campoSenha.isEmpty()){
+            Toast.makeText(LoginActivity.this
+                    , O_CAMPO_SENHA_NÃO_PODE_SER_VAZIO, Toast.LENGTH_LONG).show();
+            valido = false;
+        }
+        return valido;
     }
 }
